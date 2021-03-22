56ff97d958318557332d98eda130b395d7efa67d
Andrei <nihlete@gmail.com>
2021-03-22 17:41:27 +0300

    test 6x4

--- app/Main.hs
+++ app/Main.hs
@@ -1,23 +1,28 @@
+{-# LANGUAGE StrictData #-}
+
 module Main where
 
-import Data.Array (Array, array, assocs, bounds, indices, (!))
+import Control.Monad.State.Strict
+-- import Control.Monad.State.Lazy
+import Data.Array (Array, array, assocs, bounds, indices, ixmap, (!))
 import qualified Data.Array as Arr
 import Data.Foldable
 import Data.Ix
 import Lib
 
 main :: IO ()
-main = someFunc
+main = putStrLn $ bestResults $ buildWorldmap 6 4 // [((2, 0), RiverHead)]
 
 data Cell = River | RiverHead | Forest | Thicket | Empty deriving (Eq)
 
 instance Show Cell where
   show River = " r "
   show RiverHead = " R "
-  show Thicket = " t "
+  show Forest = " f "
+  show Thicket = " _ "
   show Empty = " _ "
 
-newtype WorldMap e = WorldMap (Array (Integer, Integer) e)
+newtype WorldMap e = WorldMap (Array (Integer, Integer) e) deriving (Eq)
 
 instance Show e => Show (WorldMap e) where
   show (WorldMap arr) = "\n" ++ concat [line i ++ ['\n'] | i <- [r0 .. r1]]
@@ -80,6 +85,8 @@ riverloop =
          ((1, 0), RiverHead)
        ]
 
+smolWorld = buildWorldmap 2 2
+
 closeCells (WorldMap arr) (i, j) = filter valid [(i + 1, j), (i, j + 1), (i -1, j), (i, j -1)]
   where
     valid pos = inRange (bounds arr) pos
@@ -91,10 +98,11 @@ possibleRiver (WorldMap arr) = if hasRiverHead then closeRiver else borderRiver
     riverHeadPos = head [fst x | x <- assocs arr, snd x == RiverHead]
     closeRiver = filter ((== Empty) . (arr !)) $ closeCells (WorldMap arr) riverHeadPos
     borderRiver =
-      range ((r0, c0), (r0, c1))
-        ++ range ((r0, c1), (r1, c1))
-        ++ range ((r0, c0), (r1, c0))
-        ++ range ((r1, c0), (r1, c1))
+      range ((r0, c0), (r0, c1 -1))
+        ++ range ((r0, c1), (r1 -1, c1))
+
+-- ++ range ((r0 + 1, c0), (r1 -1, c0)) -- по сути зеркальные и поворотные симметрии
+-- ++ range ((r1, c0), (r1, c1))
 
 propagateRiver (WorldMap arr) pos = if hasRiverHead then replace else addNew
   where
@@ -104,6 +112,59 @@ propagateRiver (WorldMap arr) pos = if hasRiverHead then replace else addNew
     addNew = WorldMap arr // [(pos, RiverHead)]
 
 propagatePossibleRivers w = map (propagateRiver w) $ possibleRiver w
+
+allRiverWorlds :: WorldMap Cell -> [WorldMap Cell]
+allRiverWorlds w = rmdub . map hideHead $ execState (changeState [w]) [w]
+  where
+    -- worlds register; last iteration
+    changeState :: [WorldMap Cell] -> State [WorldMap Cell] [WorldMap Cell]
+    changeState current = do
+      let p = concatMap propagatePossibleRivers current
+      if null p
+        then return p
+        else do
+          x <- get
+          put (x ++ p)
+          changeState p
+
+mirrorx (WorldMap arr) = WorldMap $ ixmap (bounds arr) f arr
+  where
+    ((_, c0), (_, c1)) = bounds arr
+    f (r, c) = (r, c0 + c1 - c)
+
+mirrory (WorldMap arr) = WorldMap $ ixmap (bounds arr) f arr
+  where
+    ((r0, _), (r1, _)) = bounds arr
+    f (r, c) = (r0 + r1 - r, c)
+
+removeIdentical xs = [x | x <- xs, y <- xs, not (identical x y)]
+  where
+    identical x y = (x == mirrorx y) || (x == mirrory y) || (x == (mirrorx . mirrory) y)
+
+hideHead (WorldMap arr) =
+  if hasRiverHead
+    then WorldMap arr // [(riverHeadPos, River)]
+    else WorldMap arr
+  where
+    hasRiverHead = RiverHead `elem` arr
+    riverHeadPos = head [fst x | x <- assocs arr, snd x == RiverHead]
+
+rmdub :: Eq a => [a] -> [a]
+rmdub =
+  foldl'
+    ( \seen x ->
+        if x `elem` seen
+          then seen
+          else seen ++ [x]
+    )
+    []
+
+bestResults w = concatMap (\(s, m) -> show s ++ "\n" ++ show m ++ "\n") result
+  where
+    result = filter ((== maximum score) . fst) pairs
+    pairs = zip score worlds
+    worlds = map fillWithThickets $ allRiverWorlds w
+    score = map speedBoost worlds
 
 testmap1 =
   buildWorldmap 4 4
